16e58c5a0c9f7cf38583cc0e4fafef31b271b477
theburningmonk <theburningmonk@gmail.com>
2019-09-15 20:23:59 +0200

    feat: handle out-of-order messages better
    using RXJS socket instead of ws gives better flexibility and handling of
    when messages are received out-of-orer

--- via-apigw-websockets/test/websocket.js
+++ via-apigw-websockets/test/websocket.js
@@ -1,11 +1,13 @@
 const AWS = require("aws-sdk")
 AWS.config.region = "us-east-1"
 const CloudFormation = new AWS.CloudFormation()
-const WebSocket = require("ws")
+const { ReplaySubject } = require("rxjs")
+const { webSocket } = require("rxjs/webSocket")
 
 const stackName = "e2e-test-sns-kinesis-via-apigw-e2e-test"
 
-let socket
+let subject
+let messages = new ReplaySubject(10)
 
 const connect = async () => {
 	const resp = await CloudFormation.describeStacks({
@@ -17,23 +19,22 @@ const connect = async () => {
 		.find(x => x.OutputKey === "ServiceEndpointWebsocket")
 		.OutputValue
     
-	socket = new WebSocket(endpoint)
+	subject = webSocket(endpoint)
+	subject.subscribe(data => {
+		const source = data.source
+		delete data.source
+		messages.next({ source, message: JSON.stringify(data) })
+	})
 }
 
 const disconnect = () => {
-	socket.close()
+	subject.complete()
 }
 
 const waitForMessage = (source, message) => {
 	return new Promise((resolve) => {
-		socket.on("message", function incoming(data) {
-			const incomingMessage = JSON.parse(data)
-			if (incomingMessage.source !== source) {
-				return
-			}
-      
-			delete incomingMessage.source
-			if (JSON.stringify(incomingMessage) === message) {
+		messages.subscribe(incomingMessage => {
+			if (incomingMessage.source === source && incomingMessage.message === message) {
 				resolve()
 			}
 		})
